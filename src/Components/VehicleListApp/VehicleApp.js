01069e99529b830d08fcd237333652c4633adcf7
shiva2157k <shivak916@gmail.com>
2017-07-17 12:34:53 -0400

    feat-module-link

---
+++ src/Components/VehicleListApp/VehicleApp.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import VehicleList from './../VehicleList/VehicleList'
+
+
+class VehicleApp extends Component {
+
+    constructor() {
+        super();
+        this.state = {vehicleList: []};
+    }
+
+    getInitialState() {
+        return {
+            vehicleList: {}
+        }
+    }
+
+    componentDidMount() {
+        var self = this;
+        console.log('-->Trigerring XHR');
+        const vehicleURL = "http://localhost:8080/findAll";
+        axios.get(vehicleURL)
+            .then(function (response) {
+                if (response.data) {
+                    self.setState({
+                        vehicleList: response.data
+                    })
+                }
+
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        console.log('-->Mounted in render method');
+        return (
+            <div className="VehicleApp">
+                <VehicleList vehicles={this.state.vehicleList}/>
+
+
+            </div>
+        );
+    }
+}
+
+export default VehicleApp;
